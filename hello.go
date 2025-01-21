b82884b77684c51eb65cc3f97cf2f4f6a1a720be
Monet Lee <monet_lee@163.com>
2025-01-21 16:31:59 +0800

    update b (#112)

--- hello.go
+++ hello.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// 你好
-
+	fmt.Println("hello")
+	ex, err := fmt.Println("hello")
+	fmt.Println(ex, err)
+	fmt.Println("123")
+	fmt.Println("123")
+	fmt.Println("hello")
 	fmt.Println("hello")
 }
