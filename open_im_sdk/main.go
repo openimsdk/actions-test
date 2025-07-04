a9cc80a6b16c0719f97933bf6618393f4d829c8b
Monet Lee <monet_lee@163.com>
2025-07-04 16:29:02 +0800

    12312

--- open_im_sdk/main.go
+++ open_im_sdk/main.go
@@ -1,9 +1,5 @@
 package open_im_sdk
 
-import (
-	_ "golang.org/x/mobile/bind" // 确保mobile包被保留
-)
-
 // Hello returns a greeting message
 func Hello(name string) string {
 	return "Hello " + name + " from OpenIM SDK!"
